efdda5499b9d1f19905d5b3ac2a420645c02ffa2
Mr. RG <rgirigeek@gmail.com>
2025-10-27 20:45:37 +0530

    DSA-Mathematical-Prob...

---
+++ Prob_17.java
@@ -0,0 +1,9 @@
+// Write a program that prints from 1 to 5 using for loop.
+public class Prob_17 {
+    public static void main(String[] args) {
+        System.out.print("Printing 1 to 5 using For loop : ");
+        for( int i = 1;i<=5;i++){
+            System.out.print(i +" ");
+        }
+    }
+}
